66e664c0715f20fdc13ec57febf7c3f311791dba
15838028035 <15838028035@163.com>
2019-07-12 08:13:23 +0800

    配置文件修复

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ../
-nohup java -jar jenkinsci-0.0.1-SNAPSHOT.jar -cp config/generator.properties >bin/nohup.out 2>&1 &
+nohup java -jar jenkinsci-0.0.1-SNAPSHOT.jar -cp config/generator.properties:templates/ >bin/nohup.out 2>&1 &
